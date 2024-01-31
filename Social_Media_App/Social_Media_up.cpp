a145bfa9f16aef0970db679b2eef2d6a3db09b33
01-131232-039 <01-131232-039@student.bahria.edu.pk>
2024-01-31 10:46:18 +0500

    Final One

--- Social_Media_App/Social_Media_up.cpp
+++ Social_Media_App/Social_Media_up.cpp
@@ -27,10 +27,11 @@ void Sign_up(Sign_Up_struct& sign_up_info);
 void Sign_in(Sing_in_struct& Sign_in_info, Sign_Up_struct& sign_up_info);
 void Creating_Profile(Profile_struct& profile, string& bio_choise);
 void Checking_Profile(const Profile_struct profile, string &bio_choise);
-void Main_menu(Profile_struct& profile, string& bio_choise, string idea);
+void Main_menu(Profile_struct& profile, string& bio_choise, string idea, Sing_in_struct& Sign_in_info, Sign_Up_struct& sign_up_info);
 void Editing_Profile(Profile_struct& profile, string& bio_choise);
 void Post_Array(Profile_struct& profile, string& bio_chois, string idea);
 void Watching_Post(Profile_struct& profile,  string idea);
+void Sign_Out(Sing_in_struct Sign_in_info, Sign_Up_struct sign_up_info);
 
 
 int main() {
@@ -47,7 +48,7 @@ int main() {
 	Sign_in(Sign_in_info, sign_up_info);
 	Creating_Profile(profile, bio_choise);
 	cout << "\nPress Enter To Continue...";
-	Main_menu(profile, bio_choise, idea);
+	Main_menu(profile, bio_choise, idea, Sign_in_info, sign_up_info);
 
 }
 
@@ -280,13 +281,19 @@ void Creating_Profile(Profile_struct& profile, string &bio_choise) {
 			cout << "Age is Required!\n";
 		}
 	} while (profile.YOB.empty());
-	cout << "Profile has been Succsefully Created...";
+	system("cls");
+	cout << "Profile has been Succsefully Created\n";
 	cin.clear();
+	
 
 }
 
 void Checking_Profile(Profile_struct profile, string &bio_choise) {
 	string idea;
+	Sign_Up_struct sign_up_info;
+	Sing_in_struct Sign_in_info;
+	system("cls");
+	cout << endl;
 	cout << "Your Full name is " << profile.name;
 	cout << endl;
 
@@ -301,19 +308,20 @@ void Checking_Profile(Profile_struct profile, string &bio_choise) {
 		cout << endl;
 	}
 
-	cout << "your hobbies are: " << profile.hobbies;
+	cout << "your hobbies are " << profile.hobbies;
 	cout << endl;
-	Main_menu (profile, bio_choise, idea);
+	cout << "Press Enter To Continue...";
+	Main_menu (profile, bio_choise, idea, Sign_in_info, sign_up_info);
 
 }
 
-void Main_menu(Profile_struct& profile, string& bio_choise, string idea) {
+void Main_menu(Profile_struct& profile, string& bio_choise, string idea, Sing_in_struct& Sign_in_info, Sign_Up_struct& sign_up_info) {
 	string menu_choise;
 
 	do {
 		
 		cin.ignore();
-		cout << "\nMain Menu \n1.Check Profile\n2.Editing Profile \n3.Creat a Post \n4.Watching Post \n5.Exiting the Program \nYour Choise: ";
+		cout << "\nMain Menu \n1.Check Profile\n2.Editing Profile \n3.Creat a Post \n4.Watching Post \n5.Log out \n6.Exit the Program\nYour Choise: ";
 		getline(cin, menu_choise);
 
 		if (menu_choise == "1") {
@@ -333,6 +341,9 @@ void Main_menu(Profile_struct& profile, string& bio_choise, string idea) {
 			break;
 		}
 		else if (menu_choise == "5") {
+			Sign_Out(Sign_in_info, sign_up_info);
+		}
+		else if (menu_choise == "6") {
 			cout << "\nProgram Exited\n";
 			exit;
 		}
@@ -349,27 +360,93 @@ void Main_menu(Profile_struct& profile, string& bio_choise, string idea) {
 }
 
 void Editing_Profile(Profile_struct& profile, string& bio_choise) {
+	Sign_Up_struct sign_up_info;
+	Sing_in_struct Sign_in_info;
 	string idea;
 	Creating_Profile(profile, bio_choise);
-	Main_menu(profile, bio_choise, idea);
+	Main_menu(profile, bio_choise, idea, Sign_in_info, sign_up_info);
 
 }
 
 void Post_Array(Profile_struct& profile, string& bio_chois, string idea) {
 	string bio_choise = "";
-	
+	Sign_Up_struct sign_up_info;
+	Sing_in_struct Sign_in_info;
 
 	cout << "Tell us what's new in your world! \nYour idea: ";
 	getline(cin, idea);
-	
+	if (idea.empty()) {
+		cout << "Empty Post!\n";
+		Main_menu(profile, bio_choise, idea, Sign_in_info, sign_up_info);
+	}
 	cout << "you have did a post!";
-	Main_menu(profile, bio_choise, idea);
+	Main_menu(profile, bio_choise, idea, Sign_in_info, sign_up_info);
 	
 }
 
 void Watching_Post(Profile_struct& profile, string idea) {
+	Sign_Up_struct sign_up_info;
+	Sing_in_struct Sign_in_info;
 	string bio_choise = "";
-	cout << "You have Posted: " << idea << endl;
-	cout << "Author of the Post is: " << profile.name << endl;
-	Main_menu(profile, bio_choise, idea);
+	bool e_post = true;
+	for (char empty_post : idea) {
+		if (isspace(empty_post)) {
+			e_post = false;
+			cout << "\nYou havent make any post\n";
+			cout << "Press Enter...";
+			Main_menu(profile, bio_choise, idea, Sign_in_info, sign_up_info);
+			break;
+		}
+	}
+	if (idea.empty()) {
+		cout << "\nYou havent make any post\n";
+		cout << "Press Enter...";
+		Main_menu(profile, bio_choise, idea, Sign_in_info, sign_up_info);
+		}
+	 {
+		cout << "You have Posted: " << idea << endl;
+		cout << "Author of the Post is: " << profile.name << endl;
+		cout << "Press Enter...";
+		Main_menu(profile, bio_choise, idea, Sign_in_info, sign_up_info);
+	}
 }
+void Sign_Out(Sing_in_struct Sign_in_info, Sign_Up_struct sign_up_info) {
+	string idea;
+	string bio_choise;
+	Profile_struct profile;
+	while (true)
+	{
+
+		do {
+			cout << "Your User Name: ";
+			getline(cin, Sign_in_info.user_name);
+			if (Sign_in_info.user_name.empty()) {
+				cout << "User Name is Required\n";
+			}
+		} while (Sign_in_info.user_name.empty());
+
+		do {
+			cout << "Your Password: ";
+			getline(cin, Sign_in_info.password);
+			if (Sign_in_info.password.empty()) {
+				cout << "Password is Required\n";
+			}
+		} while (Sign_in_info.password.empty());
+		if (sign_up_info.user_name != Sign_in_info.user_name) {
+			cout << "Incorrect User Name\n";
+		}
+
+		if (sign_up_info.password != Sign_in_info.password) {
+			cout << "Incorrect Password\n";
+		}
+
+		if (sign_up_info.user_name == Sign_in_info.user_name && sign_up_info.password == Sign_in_info.password) {
+			system("cls");
+			cout << "Login Successful! Welcome to Update World.\n";
+			cout << "To Continue Using the App, Please Create Your Profile.\nPress Enter Tp Continue...";
+			Main_menu(profile, bio_choise, idea, Sign_in_info, sign_up_info);
+			break;
+		}
+	}
+		
+	}
